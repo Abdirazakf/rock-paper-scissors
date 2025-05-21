94f420021353905380e33462bb2fcb85b2a2bf7e
Abdirazakf <farahabdirazak13@gmail.com>
2025-05-21 00:37:16 -0400

    Completed first iteration of the rock paper scissors game

---
+++ script.js
@@ -0,0 +1,79 @@
+function getComputerChoice(){
+    let computer_num = Math.floor(Math.random() * 3) + 1;
+    let computer_choice = null;
+
+    if (computer_num === 1){
+        computer_choice = "rock";
+    } else if (computer_num === 2){
+        computer_choice = "paper";
+    } else {
+        computer_choice = "scissors";
+    }
+
+    console.log(computer_choice);
+    return computer_choice
+}
+
+function getHumanChoice(){
+    let human_choice = prompt("Pick rock, paper, or scissors.");
+    human_choice = human_choice.toLocaleLowerCase();
+
+    console.log(human_choice);
+    return human_choice
+}
+
+function playRound(human_choice, computer_choice){
+    switch(human_choice){
+        case "rock":
+            if (computer_choice === "rock"){
+                console.log("It's a draw!");
+            } else if (computer_choice === "paper"){
+                console.log("You lose! Paper beats Rock");
+                computer_score++;
+            } else {
+                console.log("You win! Rock beats Scissors");
+                human_score++;
+            }
+            break;
+
+        case "paper":
+            if (computer_choice === "rock"){
+                console.log("You win! Paper beats Rock");
+                human_score++;
+            } else if (computer_choice === "paper"){
+                console.log("It's a draw!");
+            } else {
+                console.log("You lose! Scissors beats Paper");
+                computer_score++;
+            }
+            break;
+        
+        case "scissors":
+            if (computer_choice === "rock"){
+                console.log("You lose! Rock beats Scissors");
+                computer_score++;
+            } else if (computer_choice === "paper"){
+                console.log("You win! Scissors beats Paper");
+                human_score++;
+            } else {
+                console.log("It's a draw!");;
+            }
+            break;
+        
+        default:
+            console.log("Invalid input, Try again");
+    }
+}
+
+let computer_score = 0;
+let human_score = 0;
+let counter = 0;
+
+while (counter < 3){
+    let human_choice = getHumanChoice();
+    let computer_choice = getComputerChoice();
+    playRound(human_choice,computer_choice);
+    counter++;
+}
+
+console.log(`Final score: \n\nHuman Score: ${human_score} \nComputer Score: ${computer_score}`);
